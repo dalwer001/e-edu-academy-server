9128b102dc0ff2cdb46082bb778d21ece4845ba9
dalwer001 <dalwer.official@gmail.com>
2022-08-18 00:25:16 +0600

    added all

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.use(fileUpload());
 
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u97y4.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
